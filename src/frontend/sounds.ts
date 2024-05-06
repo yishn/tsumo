e5e4b21fbf42283222ee4f170489744c5b132d46
Yichuan Shen <shen.yichuan@gmail.com>
2024-05-06 21:35:06 +0200

    Lower sound latency

--- src/frontend/sounds.ts
+++ src/frontend/sounds.ts
@@ -1,7 +1,21 @@
-export function playTileSound() {
-  new Audio("./assets/sounds/tile.mp3").play();
+export function prepareAudio(url: string): () => void {
+  let audioContext: AudioContext | undefined;
+  const audio = new Audio(url);
+
+  return () => {
+    if (!audioContext) {
+      audioContext = new AudioContext();
+      audioContext
+        .createMediaElementSource(audio)
+        .connect(audioContext.destination);
+    }
+
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+  };
 }
 
-export function playPopSound() {
-  new Audio("./assets/sounds/pop.mp3").play();
-}
+export const playTileSound = prepareAudio("./assets/sounds/tile.mp3");
+
+export const playPopSound = prepareAudio("./assets/sounds/pop.mp3");
