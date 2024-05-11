4ce381c578ac0ac49753e867950d0c1a1a731fb9
Yichuan Shen <shen.yichuan@gmail.com>
2024-05-11 17:21:23 +0200

    Generate session id

--- src/frontend/global-state.ts
+++ src/frontend/global-state.ts
@@ -1,11 +1,22 @@
+import { uuid } from "../shared/utils.ts";
+
 const { searchParams } = new URL(location.href);
 
 export const SERVER = searchParams.get("server");
-export const SESSION = searchParams.get("session");
+export let SESSION = searchParams.get("session");
 export let SECRET = searchParams.get("secret") ?? "";
+
+if (SESSION == null) {
+  SESSION = uuid();
+  replaceHistory();
+}
 
 export function setSecret(secret: string) {
   SECRET = secret;
+  replaceHistory();
+}
+
+function replaceHistory() {
   history.replaceState(
     null,
     "",
