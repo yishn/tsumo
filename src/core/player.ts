056fd52ad4f6ac545ca4d794cf0c9e2dc6bdeb41
Yichuan Shen <shen.yichuan@gmail.com>
2024-11-02 00:41:51 +0100

    Introduce player statistics

--- src/core/player.ts
+++ src/core/player.ts
@@ -1,17 +1,48 @@
 import { Tile } from "./tile.ts";
 
+export class PlayerStatistics {
+  score = 50;
+  pongs = 0;
+  kongs = 0;
+  eats = 0;
+  stolenDiscards = 0;
+  wins = 0;
+  dealerWins = 0;
+  selfDrawnWins = 0;
+  specialHandWins = 0;
+  falseWins = 0;
+  detonatorCount = 0;
+  jokers = 0;
+  overlordCount = 0;
+}
+
 export class Player {
   tiles: Tile[] = [];
   lastDrawnTileIndex?: number;
   discards: Tile[] = [];
   melds: Tile[][] = [];
   order: [type: "discard" | "meld", index: number][] = [];
-  score: number = 50;
+  statistics = new PlayerStatistics();
+
+  get score(): number {
+    return this.statistics.score;
+  }
+
+  set score(value: number) {
+    this.statistics.score = value;
+  }
 
   getTile(index: number): Tile {
     const tile = this.tiles[index];
     if (tile == null) throw new Error("Invalid tile");
     return tile;
+  }
+
+  getAllTiles(): Tile[] {
+    return this.melds
+      .flatMap((meld) => meld)
+      .concat(this.tiles)
+      .concat(this.discards);
   }
 
   removeTiles(...indices: number[]): Tile[] {
