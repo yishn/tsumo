aaa44dc1fcf5cb45cffe0340ab804f01c2955724
Yichuan Shen <shen.yichuan@gmail.com>
2024-11-05 22:03:49 +0100

    Add useTransition

--- src/frontend/animation.ts
+++ src/frontend/animation.ts
@@ -1,15 +1,22 @@
 import { Signal, useSignal } from "sinho";
 import confetti from "canvas-confetti";
 
+export function easeOutCubic(t: number): number {
+  const t1 = t - 1;
+  return t1 * t1 * t1 + 1;
+}
+
 export function useInProgress(): [
   inProgress: Signal<boolean>,
   start: (wait: Promise<void> | number) => Promise<void>,
 ] {
   const [inProgress, setInProgress] = useSignal(false);
+  let id = 0;
 
   return [
     inProgress,
     async (wait) => {
+      const currentId = ++id;
       setInProgress(true);
 
       if (typeof wait === "number") {
@@ -18,13 +25,51 @@ export function useInProgress(): [
         await wait;
       }
 
-      setInProgress(false);
+      if (id === currentId) {
+        setInProgress(false);
+      }
     },
   ];
 }
 
 export async function delay(timeout: number): Promise<void> {
   await new Promise((resolve) => setTimeout(resolve, timeout));
+}
+
+export function useTransition(
+  transition: (t: number) => number = (t) => t
+): [
+  progress: Signal<number>,
+  inProgress: Signal<boolean>,
+  start: (duration: number) => void,
+  stop: () => void,
+] {
+  const [inProgress, setInProgress] = useSignal(true);
+  const [progress, setProgress] = useSignal(0);
+  let id = 0;
+
+  const frame = (currentId: number, startTime: number, duration: number) => {
+    const progress = transition(
+      Math.max(Math.min((Date.now() - startTime) / duration, 1), 0)
+    );
+
+    setProgress(progress);
+    if (progress >= 1) setInProgress(false);
+
+    if (inProgress() && id === currentId) {
+      requestAnimationFrame(() => frame(currentId, startTime, duration));
+    }
+  };
+
+  return [
+    progress,
+    inProgress,
+    (duration) => {
+      const currentId = ++id;
+      requestAnimationFrame(() => frame(currentId, Date.now(), duration));
+    },
+    () => setInProgress(false),
+  ];
 }
 
 export function sakuraBlossoms(): () => void {
