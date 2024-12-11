278bc9e17db0002a83054cb9dfdb0576612098c5
Yichuan Shen <shen.yichuan@gmail.com>
2024-12-11 22:41:24 +0100

    Update tutorial

--- src/frontend/tutorial/game-flow.tsx
+++ src/frontend/tutorial/game-flow.tsx
@@ -1,4 +1,3 @@
-import { TileSuit } from "../../core/tile.ts";
 import {
   DiscardIcon,
   DrawIcon,
@@ -13,22 +12,40 @@ export default () => (
     <p>At the start, every player gets 13 tiles.</p>
     <p>
       On a player’s turn, they can either{" "}
-      <span>
+      <span class="nb">
         <em>draw</em> <DrawIcon />
       </span>{" "}
       a new tile or{" "}
-      <span>
+      <span class="nb">
         <em>eat</em> <EatIcon />
       </span>{" "}
       the previous player’s discard by selecting two of their own tiles to form
-      a set. They can then proceed to declare a <em>win</em> <WinIcon /> if they
-      have a winning hand, or <em>discard</em> <DiscardIcon /> any tile on their
-      hand.
+      a set and reveal them openly. They can then proceed to declare a{" "}
+      <span class="nb">
+        <em>win</em> <WinIcon />
+      </span>{" "}
+      if they have a winning hand, or{" "}
+      <span class="nb">
+        <em>discard</em> <DiscardIcon />
+      </span>{" "}
+      any tile on their hand.
     </p>
     <p>
       Once a tile is discarded, all the other players can <em>react</em> and
-      steal the discard by forming a pong <PongIcon />, a kong
-      <KongIcon />, or a winning hand <WinIcon /> from their own hand with it.
+      steal the discard by forming a{" "}
+      <span class="nb">
+        pong <PongIcon />,
+      </span>{" "}
+      a{" "}
+      <span class="nb">
+        kong
+        <KongIcon />,
+      </span>{" "}
+      or a{" "}
+      <span class="nb">
+        winning hand <WinIcon />
+      </span>{" "}
+      from their own hand with it.
     </p>
     <p>
       If someone does, the player who reacted reveals his completed set and the
